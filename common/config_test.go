420498b5a94f655c4fc5ae840e7c3d5f0932397b
Tenyo Grozev <tenyo.grozev@yale.edu>
2021-02-28 18:55:22 -0500

    add defaultVPC config parameter per account

--- common/config_test.go
+++ common/config_test.go
@@ -10,16 +10,17 @@ var testConfig = []byte(
 	`{
 		"listenAddress": ":8000",
 		"accounts": {
-		  "provider1": {
-			"region": "us-east-1",
-			"akid": "key1",
-			"secret": "secret1"
-		  },
-		  "provider2": {
-			"region": "us-west-1",
-			"akid": "key2",
-			"secret": "secret2"
-		  }
+			"provider1": {
+				"region": "us-east-1",
+				"id": "key1",
+				"token": "secret1",
+				"defaultVPC": "vpc-123"
+			},
+			"provider2": {
+				"region": "us-west-1",
+				"id": "key2",
+				"token": "secret2"
+			}
 		},
 		"token": "SEKRET",
 		"logLevel": "info",
@@ -32,11 +33,16 @@ func TestReadConfig(t *testing.T) {
 	expectedConfig := Config{
 		ListenAddress: ":8000",
 		Accounts: map[string]Account{
-			"provider1": Account{
-				Region: "us-east-1",
+			"provider1": {
+				Region:     "us-east-1",
+				Id:         "key1",
+				Token:      "secret1",
+				DefaultVPC: "vpc-123",
 			},
-			"provider2": Account{
+			"provider2": {
 				Region: "us-west-1",
+				Id:     "key2",
+				Token:  "secret2",
 			},
 		},
 		Token:    "SEKRET",
